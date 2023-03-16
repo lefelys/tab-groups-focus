037cca7ee0895c5ff2cac0873dcd344b83c566c9
Emil Bektimirov <lefelys@gmail.com>
2023-03-16 10:58:47 +0100

    debug

--- src/background.ts
+++ src/background.ts
@@ -1,20 +1,77 @@
 // last active tabs by window ids
-let lastActiveTabs: { [key: number]: number } = {};
+
+const key = 'activeTabs';
 
 // On extension load - fill lastActiveTabs
-chrome.tabs.query({ active: true }, (tabs) => {
-  tabs.forEach((tab) => {
-    if (tab.id !== undefined) {
-      lastActiveTabs[tab.windowId] = tab.id;
+function getActiveTabs(): {} {
+  let lastActiveTabs: { [key: string]: number } = {};
+
+  chrome.tabs.query({ active: true }, (tabs) => {
+    tabs.forEach((tab) => {
+      if (tab.id !== undefined) {
+        lastActiveTabs[tab.windowId.toString()] = tab.id;
+      }
+    });
+  });
+
+  return lastActiveTabs;
+}
+
+console.log('getActiveTabs', getActiveTabs()); // 538139414 : 538139810
+console.log('stringify', JSON.stringify(getActiveTabs())); // {}
+console.log('stringify', JSON.stringify({ a: 'a' })); // {}
+console.log('!stringify', getActiveTabs()); // 538139414 : 538139810
+
+chrome.storage.local
+  .set({ activeTabs: JSON.stringify(getActiveTabs()) })
+  .then(() => {
+    //   console.log('Value is set to ', key, lastActiveTabs);
+  });
+if (chrome.runtime.lastError) {
+  console.warn(chrome.runtime.lastError);
+}
+
+chrome.storage.local.get().then((items) => {
+  // Copy the data retrieved from storage into storageCache.
+  console.log(items);
+});
+
+chrome.storage.local.get(['activeTabs']).then((result) => {
+  console.log('Value currently is', result);
+});
+
+chrome.tabs.onActivated.addListener(({ tabId, windowId }) => {
+  chrome.storage.local.get(['activeTabs'], (lastActiveTabs) => {
+    console.log(123, key, lastActiveTabs);
+
+    if (chrome.runtime.lastError) {
+      console.warn(chrome.runtime.lastError);
+    }
+
+    lastActiveTabs[windowId] = tabId;
+
+    chrome.storage.local.set({ activeTabs: lastActiveTabs });
+
+    if (chrome.runtime.lastError) {
+      console.warn(chrome.runtime.lastError);
     }
   });
 });
 
-chrome.tabs.onActivated.addListener(({ tabId, windowId }) => {
-  lastActiveTabs[windowId] = tabId;
-});
-
 chrome.tabs.onCreated.addListener(async (tab) => {
+  let lastActiveTabs: { [key: number]: number } | undefined;
+
+  chrome.storage.local.get(['activeTabs'], (result) => {
+    if (chrome.runtime.lastError) {
+      console.warn(chrome.runtime.lastError);
+    }
+
+    // console.log(result);
+    lastActiveTabs = result;
+  });
+
+  if (!lastActiveTabs) return;
+
   let prevTabId = lastActiveTabs[tab.windowId];
   if (!prevTabId) return;
 
